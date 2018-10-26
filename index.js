13c95478fdcdd53bc35e9a2eedbc8de0122ce03a
André Jaenisch <Andre.Jaenisch@cinteo.com>
2018-10-26 10:46:34 +0200

    Report URL on publish.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const ghpages = require('gh-pages');
+
+const package = require('./package.json');
  
 ghpages.publish('dist', (err) => {
     if (err) {
         console.error('Failed with', err);
         process.exit(1);
+    } else {
+        const url = getUrl();
+        console.log(`Published to ${ url }`)
     }
 });
+
+function getUrl () {
+    const name = package.name;
+    const repositoryUrl = package.repository.url;
+    const org = parseGitHubOrg(repositoryUrl);
+    return `https://${ org }.github.io/${ name }/`;
+}
+
+function parseGitHubOrg (repositoryUrl) {
+    const regex = /.*github\.com\/([\w]+)\/.*/;
+    const matches = regex.exec(repositoryUrl);
+    if (matches === null) {
+        return null;
+    } else {
+        return matches[ 1 ];
+    }
+}
